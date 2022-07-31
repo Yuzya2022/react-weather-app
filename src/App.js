16847596cc9e9cdbd66e6645a8a21c420889448d
Yuzya <iuzvyshyna.olga@gmail.com>
2022-07-31 14:21:09 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
     <h1>Weather App</h1>
     <footer>
       This project was coded by <a href="https://elaborate-cendol-df60aa.netlify.app/about.html" target="_blank" rel="noreferrer noopener">Olga Iuzvyshyna</a> and is{" "}
     <a href="https://github.com/Yuzya2022/react-weather-app" target="_blank" rel="noreferrer noopener">open-sourced on GitHub</a>
     </footer>
+    </div>
     </div>
   );
 }
