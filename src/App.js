c5de69858e5780111a136a80fd0009350e4279be
Yuzya <iuzvyshyna.olga@gmail.com>
2022-07-31 15:04:22 +0300

    Added html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-    <h1>Weather App</h1>
+    <Weather/>
     <footer>
       This project was coded by <a href="https://elaborate-cendol-df60aa.netlify.app/about.html" target="_blank" rel="noreferrer noopener">Olga Iuzvyshyna</a> and is{" "}
     <a href="https://github.com/Yuzya2022/react-weather-app" target="_blank" rel="noreferrer noopener">open-sourced on GitHub</a>
