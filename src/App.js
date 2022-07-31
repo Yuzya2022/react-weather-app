5f81f6cf71511e58ed45e641f6431c19a0ba818a
Yuzya <iuzvyshyna.olga@gmail.com>
2022-07-31 13:44:44 +0300

    Added rel="noreferrer noopener"

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
     <div className="App">
     <h1>Weather App</h1>
     <footer>
-      This project was coded by <a href="https://elaborate-cendol-df60aa.netlify.app/about.html" target="_blank">Olga Iuzvyshyna</a> and is{" "}
-    <a href="https://github.com/Yuzya2022/react-weather-app" target="_blank">open-sourced on GitHub</a>
+      This project was coded by <a href="https://elaborate-cendol-df60aa.netlify.app/about.html" target="_blank" rel="noreferrer noopener">Olga Iuzvyshyna</a> and is{" "}
+    <a href="https://github.com/Yuzya2022/react-weather-app" target="_blank" rel="noreferrer noopener">open-sourced on GitHub</a>
     </footer>
     </div>
   );
