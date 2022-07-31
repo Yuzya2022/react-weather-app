db0776f9ca295275e1359b9b126c022c6e078ce7
Yuzya <iuzvyshyna.olga@gmail.com>
2022-07-31 13:23:44 +0300

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by <a href="https://elaborate-cendol-df60aa.netlify.app/about.html" target="_blank">Olga Iuzvyshyna</a> and is{" "}
+    <a href="https://github.com/Yuzya2022/react-weather-app" target="_blank">open-sourced on GitHub</a>
+    </footer>
+    </div>
   );
 }
 
