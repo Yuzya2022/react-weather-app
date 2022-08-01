e85efb89ba7d81a67f20ef88f9bd5089f47c2b62
Yuzya <iuzvyshyna.olga@gmail.com>
2022-08-01 13:46:59 +0300

    Added Netlify link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <Weather defaultCity ="Berlin"/>
     <footer>
       This project was coded by <a href="https://elaborate-cendol-df60aa.netlify.app/about.html" target="_blank" rel="noreferrer noopener">Olga Iuzvyshyna</a> and is{" "}
-    <a href="https://github.com/Yuzya2022/react-weather-app" target="_blank" rel="noreferrer noopener">open-sourced on GitHub</a>
+    <a href="https://github.com/Yuzya2022/react-weather-app" target="_blank" rel="noreferrer noopener">open-sourced on GitHub</a> and <a href="https://gorgeous-gnome-52b55d.netlify.app/" target="_blank" rel="noreferrer noopener">hosted on Netlify</a>
     </footer>
     </div>
     </div>
