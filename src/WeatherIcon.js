bf7aaac62a78ef1cb6a9f4ce8c79d3fe895a47a9
Yuzya <iuzvyshyna.olga@gmail.com>
2022-08-02 13:48:41 +0300

    css correction

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props){
 <ReactAnimatedWeather
     icon={codeMapping[props.code]}
     color= "#212121"
-    size={54}
+    size={50}
     animate={true}
   />
     );
